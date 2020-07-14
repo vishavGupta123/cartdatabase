f4dd490982e540c1bcdbebc053ad2849cb9ff5bb
Vishav Gupta <vishavgupta2809@gmail.com>
2020-07-13 18:26:48 -0700

    new data

---
+++ index.js
@@ -0,0 +1,10 @@
+class Car {
+  constructor(name, color, doors) {
+    this.name = name;
+    this.color = color;
+    this.doors = doors;
+  }
+}
+
+const toyota = new Car("toyota", "Black", 4);
+console.log(toyota);
